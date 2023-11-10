6d69eae57c8cbcfa2f6949addd79e6a0c4a87eea
kirillsemigin <kirillsemigin@mail.ru>
2023-11-10 21:47:08 +0300

    HomeTask Ex4: GET-request

--- Ex6 Redirect.py
+++ Ex6 Redirect.py
@@ -2,7 +2,8 @@ import requests
 
 response = requests.get("https://playground.learnqa.ru/api/long_redirect")
 answer = response.history
+number_of_redirects = len(answer)
 answer2 = response.url
 
-print(answer.__len__())
+print(number_of_redirects)
 print(answer2)
