8318d7851cf4ee744793927c268393fd01297d48
kirillsemigin <kirillsemigin@mail.ru>
2023-11-27 20:56:21 +0300

    Update

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -2,7 +2,6 @@ import pytest
 import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
-from datetime import datetime
 
 
 class TestUserRegister(BaseCase):
@@ -14,20 +13,8 @@ class TestUserRegister(BaseCase):
         ('123', 'learnqa', 'learnqa', 'learnqa', None)
     ]
 
-    def setup(self):
-        base_part = 'learnqa'
-        domain = 'example.com'
-        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
-        self.email = f'{base_part}{random_part}@{domain}'
-
     def test_create_user_succesfully(self):
-        data = {
-            'password': '123',
-            'username': 'learnqa',
-            'firstName': 'learnqa',
-            'lastName': 'learnqa',
-            'email': self.email
-        }
+        data = self.prepare_registration_data()
         response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
 
         Assertions.assert_code_status(response, 200)
@@ -35,13 +22,7 @@ class TestUserRegister(BaseCase):
 
     def test_create_user_with_existing_email(self):
        email ='vinkotov@example.com'
-       data = {
-           'password': '123',
-           'username': 'learnqa',
-           'firstName': 'learnqa',
-           'lastName': 'learnqa',
-           'email': email
-       }
+       data = self.prepare_registration_data(email)
 
        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
 
