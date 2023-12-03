ce6cb9a220c9f15b1bfb1dc90ff2a6968d56b64f
kirillsemigin <kirillsemigin@mail.ru>
2023-12-03 15:08:05 +0300

    allure tags added

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -2,8 +2,9 @@ from lib.my_requests import MyRequests
 import pytest
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+import allure
 
-
+@allure.epic("Registration cases")
 class TestUserRegister(BaseCase):
     values = [
         (None, 'learnqa', 'learnqa', 'learnqa', 'vinkotov@example.com'),
@@ -13,6 +14,9 @@ class TestUserRegister(BaseCase):
         ('123', 'learnqa', 'learnqa', 'learnqa', None)
     ]
 
+    @allure.description("This test successfully creates a user")
+    @allure.feature('POSITIVE')
+    @allure.severity(allure.severity_level.CRITICAL)
     def test_create_user_succesfully(self):
         data = self.prepare_registration_data()
         response = MyRequests.post("/user/", data=data)
@@ -20,6 +24,9 @@ class TestUserRegister(BaseCase):
         Assertions.assert_code_status(response, 200)
         Assertions.assert_json_has_key(response, "id")
 
+    @allure.description("This test checks creation of a user with existing email")
+    @allure.feature('NEGATIVE')
+    @allure.severity(allure.severity_level.CRITICAL)
     def test_create_user_with_existing_email(self):
        email ='vinkotov@example.com'
        data = self.prepare_registration_data(email)
@@ -30,7 +37,9 @@ class TestUserRegister(BaseCase):
        assert response.content.decode("utf-8") == f"Users with email '{email}' already exists",\
            f'Unexpected response content {response.content}'
 
-
+    @allure.description("This test checks creation of a user with invalid email")
+    @allure.feature('NEGATIVE')
+    @allure.severity(allure.severity_level.CRITICAL)
     def test_create_user_with_invalid_email(self):
         email = 'vinkotovexample.com'
         data = {
@@ -47,6 +56,9 @@ class TestUserRegister(BaseCase):
         assert response.content.decode("utf-8") == f'Invalid email format', \
             f'Unexpected response content {response.content}'
 
+    @allure.description("This test checks creation of a user with a very short name")
+    @allure.feature('POSITIVE')
+    @allure.severity(allure.severity_level.NORMAL)
     def test_create_user_with_short_name(self):
         shortname = 'l',
         data = {
@@ -63,6 +75,9 @@ class TestUserRegister(BaseCase):
         assert response.content.decode("utf-8") == f"The value of 'username' field is too short", \
             f'Unexpected response content {response.content}'
 
+    @allure.description("This test checks creation of a user with a name more than 250 characters")
+    @allure.feature('POSITIVE')
+    @allure.severity(allure.severity_level.NORMAL)
     def test_create_user_with_long_name(self):
         longname = ('very_long_name' * 20),
         data = {
@@ -79,6 +94,9 @@ class TestUserRegister(BaseCase):
         assert response.content.decode("utf-8") == f"The value of 'username' field is too long", \
             f'Unexpected response content {response.content}'
 
+    @allure.description("This test checks creation of a user without one of the required parameter")
+    @allure.feature('NEGATIVE')
+    @allure.severity(allure.severity_level.CRITICAL)
     @pytest.mark.parametrize('password, username, firstName, lastName, email', values)
     def test_create_user_without_one_parameter(self, password, username, firstName, lastName, email):
         data = {
